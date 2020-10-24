a2f214dbe255f83504a27a921f8318c3502a238f
catherinerlopezv <catherinerlopezv@gmail.com>
2020-10-24 00:17:32 -0600

    Add files via upload

---
+++ Prueba/Archivo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba
+{
+    public class Archivo
+    {
+
+        static Encoding enc = Encoding.GetEncoding("us-ascii",
+                                          new EncoderExceptionFallback(),
+                                          new DecoderExceptionFallback());
+
+        public static void EscribirArchivoBinario(byte[] bytes, string fileName)
+        {
+            using (var crearArch = File.Create(@fileName))
+            {
+                using (var escribirbin = new BinaryWriter(crearArch, enc))
+                {
+                    escribirbin.Write(bytes);
+
+                    escribirbin.Close();
+                }
+
+                crearArch.Close();
+            }
+        }
+
+        public static byte[] GetArchivoBytes(string fileName)
+        {
+            byte[] bytes;
+
+            using (var crearArch = File.Open(fileName, FileMode.Open, FileAccess.Read))
+            {
+                using (var leerbin = new BinaryReader(crearArch, enc))
+                {
+                    bytes = leerbin.ReadBytes(Convert.ToInt32(crearArch.Length));
+
+                    leerbin.Close();
+                }
+
+                crearArch.Close();
+            }
+
+            return bytes;
+        }
+
+    }
+}
